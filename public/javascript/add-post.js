0719d817ea98b6bea4ca706a72116b960f5440e6
shanicesauce <shanicesauceda@gmail.com>
2022-10-19 14:36:25 -0500

    fixed stupid null for post_text. addpost js had content as const name instead of post_text. fixed issue

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,13 +2,13 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const content = document.querySelector('textarea[name="post-text"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
   
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
       title,
-      content,
+      post_text,
     }),
     headers: {
       'Content-Type': 'application/json',
